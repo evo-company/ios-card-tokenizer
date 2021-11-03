03f47b15ce6353e3fa7ce6f0de586be014b66855
Ilya Kostyukevich <kostilal@gmail.com>
2021-11-03 15:32:17 +0200

    update package file

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "TranzzoTokenizerTests",
-            dependencies: ["./TranzzoTokenizerTests"]),
+            dependencies: ["TranzzoTokenizerTests"]),
     ]
 )
