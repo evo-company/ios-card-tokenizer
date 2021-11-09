914c53fa88d62cbe32d70f1496203442bfd66db1
Ilya Kostyukevich <kostilal@gmail.com>
2021-11-09 11:55:45 +0200

    change target name for spm project

--- EvoPayTokenizer.podspec
+++ EvoPayTokenizer.podspec
@@ -11,6 +11,6 @@ Pod::Spec.new do |s|
 
   s.ios.deployment_target = '9.0'
 
-  s.source_files = 'TranzzoTokenizer/Sources/**/*.{h,m,swift}'
+  s.source_files = 'Sources/TranzzoTokenizer/**/*.{h,m,swift}'
   s.swift_versions = ['5.0', '5.1']
 end
--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "EvoPayTokenizer",
-            targets: ["TranzzoTokenizer"]),
+            targets: ["EvoPayTokenizer"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "TranzzoTokenizer",
+            name: "EvoPayTokenizer",
             dependencies: []),
         .testTarget(
             name: "TranzzoTokenizerTests",
-            dependencies: ["TranzzoTokenizer"]),
+            dependencies: ["EvoPayTokenizer"]),
     ]
 )
